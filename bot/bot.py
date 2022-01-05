a958893305288be7e38a4ae9dce475752eb881b2
SUKHPAL <86770159+SUKHPAL443@users.noreply.github.com>
2022-01-05 22:27:37 +0530

    Create bot.py

---
+++ bot/bot.py
@@ -0,0 +1,44 @@
+
+""" MtProto Bot """
+
+from pyrogram import (
+    Client,
+    __version__
+)
+from bot import (
+    API_HASH,
+    APP_ID,
+    LOGGER,
+    TG_BOT_TOKEN,
+    TG_BOT_WORKERS
+)
+
+
+class Bot(Client):
+    """ modded client for SESSIONSTRING """
+
+    def __init__(self):
+        super().__init__(
+            "SESSIONSTRING",
+            api_hash=API_HASH,
+            api_id=APP_ID,
+            bot_token=TG_BOT_TOKEN,
+            plugins={
+                "root": "bot/plugins"
+            },
+            workers=TG_BOT_WORKERS
+        )
+        self.LOGGER = LOGGER
+
+    async def start(self):
+        await super().start()
+        usr_bot_me = await self.get_me()
+        self.set_parse_mode("html")
+        self.LOGGER(__name__).info(
+            f"@{usr_bot_me.username} based on Pyrogram v{__version__} "
+            "Try /start."
+        )
+
+    async def stop(self, *args):
+        await super().stop()
+        self.LOGGER(__name__).info("SESSIONSTRING stopped. Bye.")
